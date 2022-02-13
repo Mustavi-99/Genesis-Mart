85f8e5df96d06d14012e5312bbb79ec0f44ba030
Mustavi-99 <mustavi.sadim99@gmail.com>
2022-02-13 11:39:11 +0600

    Login and Register From Database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,17 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Genesis_Mart.Models;
 
 namespace Genesis_Mart.Controllers
 {
     public class HomeController : Controller
     {
 
+        GenesisMartEntities db = new GenesisMartEntities();
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Customer customer)
+        {
+            if (db.Customers.Any(x => x.CUSEmail == customer.CUSEmail || x.CUSName == customer.CUSName))
+            {
+                ViewBag.Notification = "This account has already existed";
+                return View();
+            }
+            else
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+
+                Session["CUSName"] = customer.CUSName.ToString();
+                Session["CUSEmail"] = customer.CUSEmail.ToString();
+                return RedirectToAction("Index", "Home");
+
+            }
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Customer customer)
+        {
+            var checklogin = db.Customers.Where(x=>x.CUSEmail.Equals(customer.CUSEmail) && x.CUSPassword.Equals(customer.CUSPassword)).FirstOrDefault();
+            if (checklogin != null)
+            {
+                Session["CUSName"] = checklogin.CUSName;
+                Session["CUSEmail"] = customer.CUSEmail.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Wrong Email or Password";
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
